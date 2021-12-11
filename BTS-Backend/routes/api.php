1a1ff286c16e879260dca8204d4b29840e39fcea
ArionoS <arionoseptian0802@gmail.com>
2021-12-11 08:44:35 +0700

    api

--- BTS-Backend/routes/api.php
+++ BTS-Backend/routes/api.php
@@ -2,18 +2,32 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\ProductCategoryController;
+use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// TODO: products
+Route::get("products", [ProductController::class, "all"]);
+Route::get("categories", [ProductCategoryController::class, "all"]);
+
+// TODO: auth
+Route::post("register", [UserController::class, "register"]);
+Route::post("login", [UserController::class, "login"]);
+
+// TODO: user
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get("user", [UserController::class, "fetch"]);
+    Route::post("user", [UserController::class, "updateProfile"]);
+    Route::post("logout",[UserController::class, "logout"]);
+
+    Route::get("transaction", [TransactionController::class, "all"]);
+    Route::post("checkout", [TransactionController::class, "checkout"]);
 });
