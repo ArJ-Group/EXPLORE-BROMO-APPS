6d296d01b7051225de741a54395f216901967f24
ArionoS <arionoseptian0802@gmail.com>
2021-12-11 07:24:12 +0700

    routes

--- BTS-Backend/routes/web.php
+++ BTS-Backend/routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\WEB\ProductCategoryController;
+use App\Http\Controllers\WEB\DashboardController;
+use App\Http\Controllers\WEB\ProductController;
+use App\Http\Controllers\WEB\ProductGalleryController;
+use App\Http\Controllers\WEB\TransactionController;
+use App\Http\Controllers\WEB\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +21,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
+
+    Route::name('dashboard.')->prefix('dashboard')->group(function() {
+        Route::get('/', [DashboardController::class, 'index'])->name('index');
+
+        Route::middleware(['admin'])->group(function() {
+            Route::resource('product', ProductController::class);
+            Route::resource('category', ProductCategoryController::class);
+            Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only([
+                'index', 'create', 'store','destroy'
+            ]);
+            Route::resource('transaction', TransactionController::class)->only([
+                'index', 'show', 'edit', 'update'
+            ]);
+            Route::resource('user', UserController::class)->only([
+                'index', 'edit', 'update', 'destroy'
+            ]);
+        });
+    });
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
