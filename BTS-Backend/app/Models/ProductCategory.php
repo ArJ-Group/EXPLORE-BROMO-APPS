84f09665c3dc78e2745d2da524c3df336b8893b3
ArionoS <arionoseptian0802@gmail.com>
2021-12-11 09:04:11 +0700

    new

--- BTS-Backend/app/Models/ProductCategory.php
+++ BTS-Backend/app/Models/ProductCategory.php
@@ -2,20 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductCategory extends Model
 {
     use HasFactory, SoftDeletes;
-    
-    protected $fillable = [
-        'name', 
-    ];    
 
-    public function products()
-    {
-        return $this->hasMany(Product::class, 'categories_id', 'id');
+    protected $fillable = [
+       "name"
+    ];
+
+    // TODO: relasi ke table Product
+    public function products() {
+        return $this->hasMany(Product::class, "categories_id", "id");
     }
 }
--- BTS-Backend/app/Models/TransactionItem.php
+++ BTS-Backend/app/Models/TransactionItem.php
@@ -2,20 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class TransactionItem extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'users_id', 'products_id', 'transactions_id', 'quantity'
-    ]; 
+        "users_id",
+        "products_id",
+        "transactions_id",
+        "quantity",
+    ];
 
-    public function product()
-    {
-        return $this->hasOne(Product::class, 'id', 'products_id');
+    // TODO: relasi ke table user
+    public function user() {
+        return $this->belongsTo(User::class, "users_id", "id");
     }
-    
+
+    // TODO: relasi ke product
+    public function product() {
+        return $this->belongsTo(Product::class, "products_id", "id");
+    }
+
+    // TODO: relasi ke transaction
+    public function transaction() {
+        return $this->BelongsTo(Transaction::class, "transactions_id", "id");
+    }
 }
