bd6c7002fe9ad242585f30a2e2d1702fc65a7619
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-12-14 09:47:09 +0700

    FIX Controller

--- BTS-Backend/app/Http/Controllers/WEB/DashboardController.php
+++ BTS-Backend/app/Http/Controllers/WEB/DashboardController.php
@@ -4,16 +4,10 @@ namespace App\Http\Controllers\WEB;
 
 use App\Http\Controllers\Controller;
 
-use Illuminate\Http\Request;
-
 class DashboardController extends Controller
 {
-    public function index(Request $request)
-    {
-        return view('/welcome');
-    }
-    public function dashboard(Request $request)
-    {
+    // TODO: index
+    public function index() {
         return view('dashboard');
     }
 }
