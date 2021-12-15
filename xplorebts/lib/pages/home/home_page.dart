88fdbd6d798d5fcb750d5d7ba65311070cee8cd2
ExplodedRiot <alifananda66@gmail.com>
2021-12-15 11:26:58 +0700

    home page

---
+++ xplorebts/lib/pages/home/home_page.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:explore_bts/models/user_models.dart';
+import 'package:explore_bts/providers/auth_provider.dart';
+import 'package:explore_bts/providers/product_provider.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:explore_bts/theme.dart';
+import 'package:explore_bts/widget/product_card.dart';
+import 'package:explore_bts/widget/product_tile.dart';
+import 'dart:core';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AuthProvider authProvider = Provider.of<AuthProvider>(context);
+    UserModel user = authProvider.user;
+    ProductProvider productProvider = Provider.of<ProductProvider>(context);
+
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hallo, ${user.name}',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 24, fontWeight: semiBold),
+                  ),
+                  Text(
+                    '@${user.username}',
+                    style: thirdTextstyle.copyWith(fontSize: 16),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: NetworkImage(
+                    user.profilePhotoUrl!,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget categories() {
+      return Container(
+        margin: EdgeInsets.only(top: defaultMargin),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SizedBox(
+                width: defaultMargin,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: primaryColor,
+                ),
+                child: Text(
+                  'All Shoes',
+                  style: primaryTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: thirdColor),
+                  color: transparantColor,
+                ),
+                child: Text(
+                  'Running',
+                  style:
+                      thirdTextstyle.copyWith(fontSize: 13, fontWeight: medium),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: thirdColor),
+                  color: transparantColor,
+                ),
+                child: Text(
+                  'Traning',
+                  style:
+                      thirdTextstyle.copyWith(fontSize: 13, fontWeight: medium),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: thirdColor),
+                  color: transparantColor,
+                ),
+                child: Text(
+                  'Basketball',
+                  style:
+                      thirdTextstyle.copyWith(fontSize: 13, fontWeight: medium),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget popularProductsTitle() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Text(
+          'Popular Products',
+          style: primaryTextStyle.copyWith(fontSize: 22, fontWeight: semiBold),
+        ),
+      );
+    }
+
+    Widget popularProducts() {
+      return Container(
+        // height: 278,
+        margin: EdgeInsets.only(top: 14),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SizedBox(
+                width: defaultMargin,
+              ),
+              Row(
+                children: productProvider.products
+                    .map((product) => ProductCard(product))
+                    .toList(),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget newArrivalsTitle() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Text(
+          'New Arrivals',
+          style: primaryTextStyle.copyWith(fontSize: 22, fontWeight: semiBold),
+        ),
+      );
+    }
+
+    Widget newArrivals() {
+      return Container(
+        margin: EdgeInsets.only(top: 14),
+        child: Column(
+          children: productProvider.products
+              .map(
+                (product) => ProductTile(product),
+              )
+              .toList(),
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+        categories(),
+        popularProductsTitle(),
+        popularProducts(),
+        newArrivalsTitle(),
+        newArrivals()
+      ],
+    );
+  }
+}
