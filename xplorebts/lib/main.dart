8827c9a79f867a3e2193479a1e87089875a61820
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-12-15 09:43:36 +0700

    Login & Splash Pages

--- xplorebts/lib/main.dart
+++ xplorebts/lib/main.dart
@@ -1,25 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'pages/cart_page.dart';
-import 'pages/checkout_page.dart';
-import 'pages/checkout_success_page.dart';
-import 'pages/edit_profile_page.dart';
-import 'pages/home/main_page.dart';
 import 'pages/sign_in_page.dart';
-import 'pages/sign_up_page.dart';
 import 'pages/splash_page.dart';
 import 'providers/auth_provider.dart';
-import 'providers/cart_provider.dart';
-import 'providers/page_provider.dart';
 import 'providers/product_provider.dart';
-import 'providers/transaction_provider.dart';
-import 'providers/wishlist_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
@@ -30,22 +18,12 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (context) => AuthProvider()),
         ChangeNotifierProvider(create: (context) => ProductProvider()),
-        ChangeNotifierProvider(create: (context) => WishlistProvider()),
-        ChangeNotifierProvider(create: (context) => CartProvider()),
-        ChangeNotifierProvider(create: (context) => TransactionProvider()),
-        ChangeNotifierProvider(create: (context) => PageProvider()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         routes: {
           '/': (context) => SplashPage(),
           '/sign-in': (context) => SignInPage(),
-          '/sign-up': (context) => SignUpPage(),
-          '/home': (context) => MainPage(),
-          '/edit-profile': (context) => EditProfilePage(),
-          '/cart': (context) => CartPage(),
-          '/checkout': (context) => CheckoutPage(),
-          '/checkout-success': (context) => CheckoutSuccessPage(),
         },
       ),
     );
---
+++ xplorebts/lib/services/product_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:xplorebts/models/product_model.dart';
+
+class ProductService {
+  String baseUrl = 'http://192.168.1.6/api';
+
+  Future<List<ProductModel>> getProducts() async {
+    var url = '$baseUrl/products';
+    var headers = {'Content-Type': 'application/json'};
+
+    var response = await http.get(Uri.parse(url), headers: headers);
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      List data = jsonDecode(response.body)['data']['data'];
+      List<ProductModel> products = [];
+
+      for (var item in data) {
+        products.add(ProductModel.fromJson(item));
+      }
+
+      return products;
+    } else {
+      throw Exception('Gagal Get Products!');
+    }
+  }
+}
