4ce781fabfccc1f4ad56571cd4eddf101ded3023
AikaMinami <aikatan.aikachan@gmail.com>
2021-12-26 19:17:34 +0700

    Adjust color in theme.dart

--- xplorebts/lib/theme.dart
+++ xplorebts/lib/theme.dart
@@ -4,9 +4,9 @@ import 'package:google_fonts/google_fonts.dart';
 double defaultMargin = 30.0;
 
 Color primaryColor = Color(0xffE6793C);
-Color secondaryColor = Color(0xff38ABBE);
+Color secondaryColor = Color(0xffD67C49);
 Color alertColor = Color(0xffED6363);
-Color priceColor = Color(0xff2C96F1);
+Color priceColor = Color(0xffE6A826);
 
 Color backgroundColor1 = Color(0xff4D2914);
 Color backgroundColor2 = Color(0xff733D1E);
@@ -16,7 +16,7 @@ Color backgroundColor5 = Color(0xffF2813F);
 Color backgroundColor6 = Color(0xffEBCBAB);
 
 Color primaryTextColor = Color(0xffF1F0F2);
-Color secondaryTextColor = Color(0xff999999);
+Color secondaryTextColor = Color(0xff6b6b6b);
 Color subtitleColor = Color(0xff61524A);
 Color transparentColor = Colors.transparent;
 Color blackColor = Color(0xff2E2E2E);
